db0fce48af65cfeb9516b9a331f333b637d19672
Carlos Lopez <67883689+carloslomon@users.noreply.github.com>
2025-01-16 09:41:16 -0600

    Update veinsa_servidor_mvp_cedula_dos.js

--- veinsa_servidor_mvp_cedula_dos.js
+++ veinsa_servidor_mvp_cedula_dos.js
@@ -95,9 +95,9 @@ function toggle_one_img_div() {
 }
 
 
-const API_URL_IMAGE = "https://bd61-2803-6000-e007-d7-cd19-d587-faec-9146.ngrok-free.app/process-image"; // Python backend endpoint
-const API_URL_PASSWORD = "https://bd61-2803-6000-e007-d7-cd19-d587-faec-9146.ngrok-free.app/verify-password";
-const API_URL_ADD_RECORD ="https://bd61-2803-6000-e007-d7-cd19-d587-faec-9146.ngrok-free.app/quickbase-add-record";
+const API_URL_IMAGE = "https://814b-190-7-198-29.ngrok-free.app/process-image"; // Python backend endpoint
+const API_URL_PASSWORD = "https://814b-190-7-198-29.ngrok-free.app/verify-password";
+const API_URL_ADD_RECORD ="https://814b-190-7-198-29.ngrok-free.app/quickbase-add-record";
 //DIV_TMP.innerHTML += `<p>${API_URL_IMAGE}</p>`;
 //DIV_TMP.innerHTML += `<p>${API_URL_PASSWORD}</p>`;
 //DIV_TMP.innerHTML += `<p>${API_URL_ADD_RECORD}</p>`;
