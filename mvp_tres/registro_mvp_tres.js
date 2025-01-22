3ce821715d8c987c0994e84a26814a4b32cc2c04
Carlos Lopez <67883689+carloslomon@users.noreply.github.com>
2025-01-22 16:28:25 -0600

    Update registro_mvp_tres.js

--- mvp_tres/registro_mvp_tres.js
+++ mvp_tres/registro_mvp_tres.js
@@ -1,6 +1,6 @@
 
 let API_URL_ADD_RECORD_CLIENTES = "https://dc48-2803-6000-e007-d7-49b7-2ffd-6c59-d136.ngrok-free.app/quickbase-add-record-clientes-uno";
-API_URL_CEDULA_JURIDICA = sharedHttpPoint + "https://dc48-2803-6000-e007-d7-49b7-2ffd-6c59-d136.ngrok-free.app/process-cedula-juridica";
+let API_URL_CEDULA_JURIDICA = "https://dc48-2803-6000-e007-d7-49b7-2ffd-6c59-d136.ngrok-free.app/process-cedula-juridica";
 
 let DIV_TMP = document.getElementById("url-div");
 
